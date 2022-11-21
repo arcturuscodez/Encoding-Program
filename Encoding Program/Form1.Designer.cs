9bb4ccd79d465a460b265a94a329df060c399b36
Sonny <43947002+arcturuscodez@users.noreply.github.com>
2022-11-21 21:07:23 +0200

    Add files via upload

---
+++ Encoding Program/Form1.Designer.cs
@@ -0,0 +1,255 @@
+﻿namespace Encoding_Program
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.ExportBox1 = new System.Windows.Forms.TextBox();
+            this.ImportBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.ImportBox2 = new System.Windows.Forms.TextBox();
+            this.ExportBox2 = new System.Windows.Forms.TextBox();
+            this.eventLog1 = new System.Diagnostics.EventLog();
+            this.ResetButton1 = new System.Windows.Forms.Button();
+            this.StatusBox1 = new System.Windows.Forms.TextBox();
+            this.StatusLabel = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.eventLog1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(12, 57);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(138, 61);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Encode";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // ExportBox1
+            // 
+            this.ExportBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ExportBox1.Location = new System.Drawing.Point(432, 74);
+            this.ExportBox1.Name = "ExportBox1";
+            this.ExportBox1.ReadOnly = true;
+            this.ExportBox1.Size = new System.Drawing.Size(250, 29);
+            this.ExportBox1.TabIndex = 2;
+            // 
+            // ImportBox1
+            // 
+            this.ImportBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ImportBox1.Location = new System.Drawing.Point(156, 74);
+            this.ImportBox1.Name = "ImportBox1";
+            this.ImportBox1.Size = new System.Drawing.Size(250, 29);
+            this.ImportBox1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(236, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 25);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Import";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(522, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 25);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Export";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(12, 177);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(138, 61);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Decode";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(522, 168);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 25);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Export";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(236, 168);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(66, 25);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Import";
+            // 
+            // ImportBox2
+            // 
+            this.ImportBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ImportBox2.Location = new System.Drawing.Point(156, 196);
+            this.ImportBox2.Name = "ImportBox2";
+            this.ImportBox2.Size = new System.Drawing.Size(250, 29);
+            this.ImportBox2.TabIndex = 8;
+            this.ImportBox2.TextChanged += new System.EventHandler(this.ImportBox2_TextChanged);
+            // 
+            // ExportBox2
+            // 
+            this.ExportBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ExportBox2.Location = new System.Drawing.Point(432, 196);
+            this.ExportBox2.Name = "ExportBox2";
+            this.ExportBox2.ReadOnly = true;
+            this.ExportBox2.Size = new System.Drawing.Size(250, 29);
+            this.ExportBox2.TabIndex = 7;
+            // 
+            // eventLog1
+            // 
+            this.eventLog1.SynchronizingObject = this;
+            // 
+            // ResetButton1
+            // 
+            this.ResetButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ResetButton1.Location = new System.Drawing.Point(722, 410);
+            this.ResetButton1.Name = "ResetButton1";
+            this.ResetButton1.Size = new System.Drawing.Size(75, 37);
+            this.ResetButton1.TabIndex = 12;
+            this.ResetButton1.Text = "Reset";
+            this.ResetButton1.UseVisualStyleBackColor = true;
+            this.ResetButton1.Click += new System.EventHandler(this.ResetButton1_Click);
+            // 
+            // StatusBox1
+            // 
+            this.StatusBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.StatusBox1.Location = new System.Drawing.Point(356, 313);
+            this.StatusBox1.Multiline = true;
+            this.StatusBox1.Name = "StatusBox1";
+            this.StatusBox1.ReadOnly = true;
+            this.StatusBox1.Size = new System.Drawing.Size(326, 134);
+            this.StatusBox1.TabIndex = 13;
+            this.StatusBox1.TextChanged += new System.EventHandler(this.StatusBox1_TextChanged);
+            // 
+            // StatusLabel
+            // 
+            this.StatusLabel.AutoSize = true;
+            this.StatusLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.StatusLabel.Location = new System.Drawing.Point(352, 288);
+            this.StatusLabel.Name = "StatusLabel";
+            this.StatusLabel.Size = new System.Drawing.Size(61, 22);
+            this.StatusLabel.TabIndex = 14;
+            this.StatusLabel.Text = "Status";
+            this.StatusLabel.Click += new System.EventHandler(this.StatusLabel_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(12, 313);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(299, 134);
+            this.textBox1.TabIndex = 15;
+            this.textBox1.Text = resources.GetString("textBox1.Text");
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(12, 288);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(97, 22);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Helpful Tip";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.StatusLabel);
+            this.Controls.Add(this.StatusBox1);
+            this.Controls.Add(this.ResetButton1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.ImportBox2);
+            this.Controls.Add(this.ExportBox2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ImportBox1);
+            this.Controls.Add(this.ExportBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Encoding Program";
+            ((System.ComponentModel.ISupportInitialize)(this.eventLog1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox ExportBox1;
+        private System.Windows.Forms.TextBox ImportBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox ImportBox2;
+        private System.Windows.Forms.TextBox ExportBox2;
+        private System.Diagnostics.EventLog eventLog1;
+        private System.Windows.Forms.Button ResetButton1;
+        private System.Windows.Forms.Label StatusLabel;
+        private System.Windows.Forms.TextBox StatusBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Encoding Program/Form1.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace Encoding_Program
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Encode Button
+
+            string InputString;
+            InputString = ImportBox1.Text;
+            bool b = false;
+
+            StatusBox1.Text = string.Empty;
+            
+            //check for bad passwords
+            if (InputString.Equals("password"))
+            {
+                StatusBox1.Text = "This passcode is too common and will not securely protect your account. Use another passcode.";
+                return;
+            }
+
+            b = Regex.IsMatch(InputString, "[A-Z]");
+            if (b == false)
+            {
+                StatusBox1.Text = "Please provide a password with an uppercase letter!";
+                return;
+            }
+
+            b = Regex.IsMatch(InputString, "[0-9]");
+            if (b == false)
+            {
+                StatusBox1.Text = "Please provide a password with a number!";
+                return;
+            }
+
+            if (InputString.IndexOf("!") < 0) 
+            {
+                StatusBox1.Text = "Please provide a password with an exclamation mark!";
+                return;
+            }
+
+            if (InputString.Length < 6)
+            {
+                StatusBox1.Text = "Please provide a password with at least 6 characters!";
+                return;
+            }
+
+            if(InputString.Length >= 20)
+            {
+                StatusBox1.Text = "Please provide a password with less than 20 characters!";
+                return;
+            }
+
+            ExportBox1.Text = EncodingProcess (InputString);
+
+            //Sends new code to decode
+
+            ImportBox2.Text = ExportBox1.Text;
+
+            
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //Decode Button
+
+            string DecodeString;
+            DecodeString = ImportBox2.Text;
+            ExportBox2.Text = DecodingProcess (DecodeString);
+        }
+
+      
+        private string EncodingProcess (string S)
+        {
+            string OutputString;
+            OutputString = "";
+            
+
+            char[] charlist = S.ToCharArray();
+            char tmp;
+            char[] newcharlist = new char[25];
+            int index = 0;
+            foreach (char c in charlist)
+            {
+                if (index >= 20) //stops code from breaking when exceeding 25 characters 
+                {
+                    StatusBox1.Text = "You have exceeded the maximum amount of characters. You need to specify less than 20 characters. Please reset the form using the reset button!";
+                    break;
+                }
+
+                tmp = (char) (c + 3 - 5 );
+                newcharlist[index] = tmp;
+                index = index + 1;
+                
+            }
+
+            OutputString = new string(newcharlist);
+            
+
+            return OutputString;
+
+        }
+
+       
+        private string DecodingProcess(string S)
+        {
+            string OutputString;
+            OutputString = "";
+
+
+            char[] charlist = S.ToCharArray();
+            char tmp;
+            char[] newcharlist = new char[25];
+            int index = 0;
+            foreach (char c in charlist)
+            {
+                if (index >= 25) //stops code from breaking when exceeding 25 characters 
+                {
+                    break;
+                }
+
+                tmp = (char)(c - 3 + 5 );
+                newcharlist[index] = tmp;
+                index = index + 1;
+
+            }
+
+            OutputString = new string(newcharlist);
+
+
+            return OutputString;
+
+        }
+
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            // Not Important
+        }
+
+        private void ImportBox2_TextChanged(object sender, EventArgs e)
+        {
+            // Not Important
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ResetButton1_Click(object sender, EventArgs e)
+        {
+            //Resets values to nothing
+
+            ExportBox1.Text = string.Empty;
+            ExportBox2.Text = string.Empty;
+            ImportBox1.Text = string.Empty;
+            ImportBox2.Text = string.Empty;
+            StatusBox1.Text = string.Empty;
+        }
+
+        private void StatusLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void StatusBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
